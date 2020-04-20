39bbc89aad41fb94ec2616d6819352c347ea6ed4
EC2 Default User <ec2-user@ip-172-31-37-107.us-east-2.compute.internal>
2020-04-20 18:00:06 +0000

    feat: create update delete added

--- deploy.py
+++ deploy.py
@@ -1,13 +1,40 @@
+#!/usr/bin/python3
+import getopt, sys
+
+argument_list = sys.argv[1:]
+short_options = "cdus"
+long_options = ["create", "delete", "update", "set"]
+
+try:
+    arguments, values = getopt.getopt(argument_list, short_options, long_options)
+    command = arguments[0][0]
+except getopt.error as err:
+    print (str(err))
+    sys.exit(2)
+    
+################################################################################
 import boto3
 PROJECT = 'AWS-CloudFormation'
 cloudformation = boto3.client('cloudformation')
 
 with open('template.yaml','r') as file:
     template = file.read()
+if command in ('--create','-c'):
+    response = cloudformation.create_stack(
+        StackName=PROJECT,
+        TemplateBody=template
+    )
+    print(response)
+elif command in ("--update", "-u"):
 
-response = cloudformation.create_stack(
-    StackName=PROJECT,
-    TemplateBody=template
-)
+    response = cloudformation.update_stack(
+        StackName=PROJECT,
+        TemplateBody=template
+    )
+    print(response)
 
-print(response)
+elif command in ("--delete", "-d"):
+    response = cloudformation.delete_stack(
+        StackName=PROJECT
+    )
+    print(response)
